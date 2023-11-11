1eec662c835b5080830e75c60d415bdc25f1abb1
Tysonpower <support@tynet.eu>
2023-11-11 18:24:20 +0100

    Messages are now in folders per date

--- main/includes/customize.h
+++ main/includes/customize.h
@@ -2,7 +2,7 @@
 #include "sdkconfig.h"
 
 #define HW_VERSION "2023.01"
-#define FW_VERSION "1.0.6"
+#define FW_VERSION "1.0.7"
 
 #define EXAMPLE_ESP_WIFI_SSID      "Othernet"
 #define EXAMPLE_ESP_WIFI_PASS      "othernet"
